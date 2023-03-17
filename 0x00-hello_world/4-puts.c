3aef72839dfc8e536f5a1e0845c4679821d69f99
Mkpos <emeraldjimmy500@gmail.com>
2023-03-16 19:13:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * print:str:"programming is like building a multilingual puzzel"
- * return 0 (success)
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
