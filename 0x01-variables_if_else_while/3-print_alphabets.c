60cf540a97f4218c8a1a1100e704b297f5580621
Mkpos <emeraldjimmy500@gmail.com>
2023-03-18 15:29:54 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- *
- * Result: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	char x;
+
 	for (x = 'a' ; x <= 'z' ; x++)
 		putchar(x);
 	for (x = 'A' ; x <= 'Z' ; x++)
