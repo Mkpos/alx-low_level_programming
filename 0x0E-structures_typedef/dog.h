2409d65ab402ed7b8c17ca5cced9a88f359dbae2
Mkpos <emeraldjimmy500@gmail.com>
2023-04-17 12:20:33 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - This will serve as a prototype describing dog
+ * @name: Name of the dog to be stored.
+ * @age: Age of the dog to be stored.
+ * @owner: Owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
