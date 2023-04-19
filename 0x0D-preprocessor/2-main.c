78e1bfa81cedd96cf22fbddb8c842758d51d94d2
Mkpos <emeraldjimmy500@gmail.com>
2023-04-18 19:24:36 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints the name of the file it was compil
+ * ed from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
